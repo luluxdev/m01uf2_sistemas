e53f3d15831391a9a832f5d31473537c125688f7
lucia_puga <lucia.puga@enti.cat>
2024-11-20 11:25:37 +0100

    Actualización clase 20/11

--- server.sh
+++ server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 PORT="2022"
 
 echo "Servidor de Dragón Magia Abuelita Miedo 2022"
@@ -8,35 +9,40 @@ echo "0. ESCUCHAMOS"
 
 DATA=`nc -l $PORT`
 
-if [ "$DATA" != "DMAM" ]
+HEADER=`echo "$DATA" | cut -d " " -f 1`
+IP=`echo $DATA | cut -d " " -f 2`
+
+if [ "$HEADER" != "DMAM" ]
 then
 	echo "ERROR 1: Cabecera incorrecta"
-	echo "KO_HEADER" | nc localhost $PORT
+	echo "KO_HEADER" | nc $IP $PORT
 	exit 1
 fi
 
+echo "La IP del cliente es: $IP"
+
 echo "2. CHECK OK - Enviando OK_HEADER"
-echo "OK_HEADER" | nc localhost $PORT
+echo "OK_HEADER" | nc $IP $PORT
 DATA=`nc -l $PORT`
 
 echo "5. COMPROBANDO PREFIJO"
 
 PREFIX=`echo "$DATA" | cut -d ' ' -f 1`
-FILE_NAME=`echo "$DATA" | cut -d ' ' -f 2` # Extraemos el nombre del archivo
+FILE_NAME=`echo "$DATA" | cut -d ' ' -f 2`
 
 if [ "$PREFIX" != "FILE_NAME" ]
 then
 	echo "ERROR 2: Prefijo incorrecto"
-	echo "KO_FILE_NAME" | nc localhost $PORT
+	echo "KO_FILE_NAME" | nc $IP $PORT
 	exit 2
 fi
 
 echo "6. ENVIANDO OK_FILE_NAME"
 
-echo "OK_FILE_NAME" | nc localhost $PORT
+echo "OK_FILE_NAME" | nc $IP $PORT
 
 DATA=`nc -l $PORT`
 
 echo "9. Recibiendo el dragón"
 mkdir -p server
-echo "$DATA" > "server/$FILE_NAME" # Usa el nombre recibido para guardar el archivo
+echo "$DATA" > "server/$FILE_NAME"
