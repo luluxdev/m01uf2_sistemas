2ff462b0604287d35d361a639c371dc4e83973a0
lucia_puga <lucia.puga@enti.cat>
2025-01-17 12:49:44 +0100

    Actualización final de la UF2!

--- client.sh
+++ client.sh
@@ -2,10 +2,10 @@
 
 if [ "$1" == "" ]
 then
-	echo "Debes indicar la dirección del servidor."
-	echo "Ejemplo:"
-	echo -e "\t$0 127.0.0.1"
-	exit 1
+    echo "Debes indicar la dirección del servidor."
+    echo "Ejemplo:"
+    echo -e "\t$0 127.0.0.1"
+    exit 1
 fi
 
 IP_SERVER=$1
@@ -24,25 +24,41 @@ DATA=`nc -l $PORT`
 echo "3. COMPROBANDO HEADER"
 if [ "$DATA" != "OK_HEADER" ] 
 then
-	echo "ERROR 1: el header se envió incorrectamente"
-	exit 1
+    echo "ERROR 1: el header se envió incorrectamente" >&2
+    exit 1
 fi
 
 echo "4. Enviando el FILE_NAME"
 
 FILE_NAME="dragon.txt"
+FILE_NAME_MD5=`echo -n "$FILE_NAME" | md5sum | cut -d ' ' -f 1`
 
-echo "FILE_NAME $FILE_NAME" | nc $IP_SERVER $PORT
+echo "FILE_NAME $FILE_NAME $FILE_NAME_MD5" | nc $IP_SERVER $PORT
 
 echo "7. RECIBIENDO COMPROBACIÓN FILE_NAME"
 DATA=`nc -l $PORT`
 
 if [ "$DATA" != "OK_FILE_NAME" ]
 then
-	echo "ERROR 2: el nombre se envió incorrectamente"
-	exit 2
+    echo "ERROR 2: el nombre o su hash se enviaron incorrectamente" >&2
+    exit 2
 fi
 
 echo "8. ENVIANDO CONTENIDO"
 
 cat "client/$FILE_NAME" | nc $IP_SERVER $PORT
+
+echo "11. ENVIANDO HASH DEL CONTENIDO"
+
+FILE_CONTENT_MD5=`md5sum "client/$FILE_NAME" | cut -d ' ' -f 1`
+echo "FILE_MD5 $FILE_CONTENT_MD5" | nc $IP_SERVER $PORT
+
+DATA=`nc -l $PORT`
+
+if [ "$DATA" != "OK_FILE_MD5" ]
+then
+    echo "ERROR 3: el contenido o su hash no coincidieron" >&2
+    exit 3
+fi
+
+echo "13. PROCESO COMPLETADO CORRECTAMENTE"
--- server.sh
+++ server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 PORT="2022"
 
 echo "Servidor de Dragón Magia Abuelita Miedo 2022"
@@ -14,9 +13,9 @@ IP=`echo $DATA | cut -d " " -f 2`
 
 if [ "$HEADER" != "DMAM" ]
 then
-	echo "ERROR 1: Cabecera incorrecta"
-	echo "KO_HEADER" | nc $IP $PORT
-	exit 1
+    echo "ERROR 1: Cabecera incorrecta" >&2
+    echo "KO_HEADER" | nc $IP $PORT
+    exit 1
 fi
 
 echo "La IP del cliente es: $IP"
@@ -29,16 +28,25 @@ echo "5. COMPROBANDO PREFIJO"
 
 PREFIX=`echo "$DATA" | cut -d ' ' -f 1`
 FILE_NAME=`echo "$DATA" | cut -d ' ' -f 2`
+RECEIVED_MD5=`echo "$DATA" | cut -d ' ' -f 3`
 
 if [ "$PREFIX" != "FILE_NAME" ]
 then
-	echo "ERROR 2: Prefijo incorrecto"
-	echo "KO_FILE_NAME" | nc $IP $PORT
-	exit 2
+    echo "ERROR 2: Prefijo incorrecto" >&2
+    echo "KO_FILE_NAME" | nc $IP $PORT
+    exit 2
+fi
+
+GENERATED_MD5=`echo -n "$FILE_NAME" | md5sum | cut -d ' ' -f 1`
+
+if [ "$GENERATED_MD5" != "$RECEIVED_MD5" ]
+then
+    echo "ERROR 3: Hash del nombre de archivo incorrecto" >&2
+    echo "KO_FILE_NAME_MD5" | nc $IP $PORT
+    exit 3
 fi
 
 echo "6. ENVIANDO OK_FILE_NAME"
-
 echo "OK_FILE_NAME" | nc $IP $PORT
 
 DATA=`nc -l $PORT`
@@ -46,3 +54,28 @@ DATA=`nc -l $PORT`
 echo "9. Recibiendo el dragón"
 mkdir -p server
 echo "$DATA" > "server/$FILE_NAME"
+
+echo "10. ESPERANDO HASH DEL CONTENIDO"
+
+DATA=`nc -l $PORT`
+PREFIX=`echo "$DATA" | cut -d ' ' -f 1`
+RECEIVED_FILE_MD5=`echo "$DATA" | cut -d ' ' -f 2`
+
+if [ "$PREFIX" != "FILE_MD5" ]
+then
+    echo "ERROR 4: Prefijo incorrecto para hash del contenido" >&2
+    echo "KO_FILE_MD5" | nc $IP $PORT
+    exit 4
+fi
+
+GENERATED_FILE_MD5=`md5sum "server/$FILE_NAME" | cut -d ' ' -f 1`
+
+if [ "$GENERATED_FILE_MD5" != "$RECEIVED_FILE_MD5" ]
+then
+    echo "ERROR 5: Hash del contenido no coincide" >&2
+    echo "KO_FILE_MD5" | nc $IP $PORT
+    exit 5
+fi
+
+echo "12. ENVIANDO OK_FILE_MD5"
+echo "OK_FILE_MD5" | nc $IP $PORT
